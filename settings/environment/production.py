2116890adf2049be15345986944c72218ce08bad
codefather <codefather.ru@gmail.com>
2022-07-16 17:23:38 +0300

    ci fix

--- settings/environment/production.py
+++ settings/environment/production.py
@@ -1,21 +1,19 @@
 import ast
 
-from settings.environment.local import *
+from settings.environment.base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG")))
-HOST = str(os.environ.get("HOST"))
 
+HOST = str(os.environ.get("HOST"))
 ALLOWED_HOSTS = list(ast.literal_eval(os.environ.get("ALLOWED_HOSTS")))
 ADMIN_ROUTER_ENABLED = bool(int(os.environ.get("ADMIN_ROUTER_ENABLED", "0")))
 
 # Database Settings
 # ------------------------------------------------------------------------------
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -30,18 +28,12 @@ DATABASES = {
 
 # CORS Settings
 # ------------------------------------------------------------------------------
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 CSRF_TRUSTED_ORIGINS = list(ast.literal_eval(os.environ.get('CSRF_TRUSTED_ORIGINS')))
-CSRF_COOKIE_SAMESITE = None
-CSRF_COOKIE_HTTPONLY = True
-CSRF_USE_SESSIONS = True
-
-CORS_ALLOW_ALL_ORIGINS = bool(int(os.environ.get("CORS_ALLOW_ALL_ORIGINS", '1')))
-CORS_ORIGIN_ALLOW_ALL = bool(int(os.environ.get("CORS_ORIGIN_ALLOW_ALL", '1')))
 CORS_ORIGIN_WHITELIST = list(ast.literal_eval(os.environ.get('CSRF_TRUSTED_ORIGINS')))
-
 CORS_ALLOWED_ORIGINS = list(ast.literal_eval(os.environ.get('CSRF_TRUSTED_ORIGINS')))
 
+# Checking Settings
+# ------------------------------------------------------------------------------
 django_settings_error = check_django_settings()
 system_message(django_settings_error) if django_settings_error else None
 
