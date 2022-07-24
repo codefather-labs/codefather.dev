b22318fc6d4640ea381a28375f2c9690fbf39caa
codefather <codefather.ru@gmail.com>
2022-07-24 15:08:22 +0300

    New title font

--- apps/core/management/commands/load_markdown_post.py
+++ apps/core/management/commands/load_markdown_post.py
@@ -1,6 +1,8 @@
 import os
 
 from django.core.management import BaseCommand
+from django.utils.text import slugify
+
 from settings.environment.settings import get_settings_module
 
 from apps.core.models import Post
@@ -11,35 +13,35 @@ BASE_DIR = settings.BASE_DIR
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('post_dir', nargs='+', type=str)
+        parser.add_argument('post_file', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        post_dir = options['post_dir'][0]
-        post_file = ""
+        post_file = options['post_file'][0]
+        post_name = "".join(str(post_file)
+                            .rsplit("/", maxsplit=1)[-1]
+                            .split(".")[:-1]) \
+            .replace("_", " ")
+        print(post_name)
+        post_dir = "/".join(str(post_file).split("/")[:-1])
 
-        for file in os.listdir(post_dir):
-            if file[-3:] == '.md':
-                post_file = file
-
-        post_text = open(f"{post_dir}/{post_file}", 'r').read()
+        with open(f"{post_file}", 'r', encoding='utf-8') as p_file:
+            post_text = p_file.read()
 
         path = lambda x, y: f"{x}/media/{y}/"
-        os.system(f"mkdir media/post")
+        os.system("mkdir media/post")
         os.system(f"mkdir {path(settings.BASE_DIR, post_dir)}")
 
         for file in os.listdir(post_dir):
             if file[-3:] not in ('.md', '.DS_Store'):
                 # image_file_path = path(settings.BASE_DIR, f"{post_dir}/{file}")
                 image_file_path = f"{settings.HOST}/media/{post_dir}/{file}"
-                post_text = post_text.replace(
-                    file, image_file_path
-                )
+                post_text = post_text.replace(file, image_file_path)
 
                 command = f"cp -r {post_dir}/{file} {settings.BASE_DIR}/media/{post_dir}/"
                 os.system(command)
 
         Post.objects.create(
-            title="Python Internals",
-            slug="python-internals",
+            title=post_name,
+            slug=slugify(post_name, allow_unicode=True),
             markdown=post_text,
         )
